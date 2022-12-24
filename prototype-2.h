50bda2167438ec291a0bc09218ad520b1566d74a
Jayasree1802 <121030796+Jayasree1802@users.noreply.github.com>
2022-12-24 08:17:00 +0530

    Update prototype-2.h

--- prototype-2.h
+++ prototype-2.h
@@ -43,37 +43,39 @@ typedef struct product_details
 }product_details;
 
 
-
 void admin();
-seller_Details *  price_check(int ,int ,seller_Details *);
-seller_Details *create_lisr_of_seller(seller_Details *,seller_Details);
-seller_Details *file_to_list(seller_Details *);
-seller_Details question_extraction_from_file(char * , seller_Details );
 
+//functions of seller menu
 
+seller_Details *  price_check(int ,int ,seller_Details *);                       
+seller_Details *create_lisr_of_seller(seller_Details *,seller_Details);           //function to create list of seller
+seller_Details *file_to_list(seller_Details *);                                
+seller_Details seller_extraction_from_file(char * , seller_Details );
 void design() ;
 
-seller_Details *seller(seller_Details * , product_details *);
+seller_Details *seller(seller_Details * , product_details *);                    
 
-seller_Details *login_seller(seller_Details *,char * , char *);
+seller_Details *login_seller(seller_Details *,char * , char *);                 //function to login the seller
 
 void check_the_buyer_request(seller_Details *);
 
 seller_Details * login(seller_Details *);
 int new_registration_to_file();
 
-product_details *product_registration(product_details *);
+//functions of product menu
 
-product_details data_extraction_from_buffer(char * , product_details);
+product_details *product_registration(product_details *);                        //functions to register the products
 
-product_details *create_list_for_product(product_details * , product_details);
+product_details data_extraction_from_buffer(char * , product_details);           
+
+product_details *create_list_for_product(product_details * , product_details);    //creating the list for products  
 
 product_details *delete_product(product_details * ,int);
 
-product_details * modify_product_details(product_details * ,int,seller_Details *);
-void display_product(product_details *);
+product_details * modify_product_details(product_details * ,int,seller_Details *);   //function to modify details of products
+void display_product(product_details *);                                             //function to display products
 
-void adding_produt_into_file();
+void adding_produt_into_file();                                                      //function to adding products into file
 
 
 
