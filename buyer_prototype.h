2966999b1badae789b16e0ab4967a54cf14413b8
Jayasree1802 <121030796+Jayasree1802@users.noreply.github.com>
2022-12-24 08:11:30 +0530

    Update buyer_prototype.h

--- buyer_prototype.h
+++ buyer_prototype.h
@@ -1,5 +1,7 @@
 #include<stdio.h>
 
+//Structure for Buyer Details
+
 typedef struct buyer_Details
 {
         int id;
@@ -16,18 +18,18 @@ typedef struct buyer_Details
         
 }buyer_Details;
 
+//functions for buyer login
 
+buyer_Details *buyer(buyer_Details * , seller_Details *);                         
+buyer_Details *registration_buyer(buyer_Details *);                       // function for registration of buyer
+buyer_Details *buyer_login(buyer_Details *,char * , char *);              //function for buyer login
+seller_Details *price_check(int ,int ,seller_Details *);                 //check the price for the seller
+void writing_buyer_info_to_file();                                       // Buyer information in file
 
-buyer_Details *buyer(buyer_Details * , seller_Details *);
-buyer_Details *registration_buyer(buyer_Details *);
-buyer_Details *buyer_login(buyer_Details *,char * , char *);
-seller_Details *price_check(int ,int ,seller_Details *);
-void writing_buyer_info_to_file();
-
-buyer_Details data_extraction_from_buyer_file(char * ,buyer_Details);
-buyer_Details *create_list_buyer(buyer_Details * , buyer_Details);
+buyer_Details data_extraction_from_buyer_file(char * ,buyer_Details);     //Extracting data from buyer file
+buyer_Details *create_list_buyer(buyer_Details * , buyer_Details);        //create a linked list to the  buyer
 void display(seller_Details *);
 
-void auction_for_product(product_details * , seller_Details * , buyer_Details *);
+void auction_for_product(product_details * , seller_Details * , buyer_Details *);    // auction function to make bidding
 
-product_details *get_the_product_detail(seller_Details * ,int);
+product_details *get_the_product_detail(seller_Details * ,int);                     // display the product details
