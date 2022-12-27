47f55ea41d28660524ca3235cea01fb7de568760
Jayasree1802 <121030796+Jayasree1802@users.noreply.github.com>
2022-12-28 05:07:48 +0530

    Update client1.c

--- client1.c
+++ client1.c
@@ -1,10 +1,18 @@
-//client.c
+// definition file for client
+
+//header files
 
 #include<stdio.h>
 #include<stdlib.h>
 #include<prototype.h>
 #include<buyer-prototype.h>
 
+/*******************************************************************************************
+**  FUNCTION NAME   : main()
+**  DESCRIPTION     : It is the function display the main menu of the application
+**  PARAMETERS      : No Parameters
+**  RETURN          :  No Return 
+*******************************************************************************************/
 
 
 int main()
